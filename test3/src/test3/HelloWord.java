5cff2cc2915feee88b2d187bf03ab0601abe29e1
Administrator <Administrator@172.31.28.153>
2016-01-29 21:43:19 +0000

    My First Commit

---
+++ test3/src/test3/HelloWord.java
@@ -0,0 +1,13 @@
+package test3;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world!");
+
+
+	}
+
+}
